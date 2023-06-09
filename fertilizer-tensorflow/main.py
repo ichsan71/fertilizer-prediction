18e3508750c8d3491ecb7ca7f7e3942fd12d205a
Adnan Al-farizi Rahmat <42133636+alfa000@users.noreply.github.com>
2023-06-09 20:39:03 +0700

    feat : docker file

--- fertilizer-tensorflow/main.py
+++ fertilizer-tensorflow/main.py
@@ -6,7 +6,7 @@ from tensorflow import keras
 
 app = Flask(__name__)
 options = tf.saved_model.LoadOptions(experimental_io_device='/job:localhost')
-model = tf.keras.models.load_model('save_model/1', options=options)
+model = tf.keras.models.load_model('./save_model/1', options=options)
 
 @app.route("/")
 def home():
